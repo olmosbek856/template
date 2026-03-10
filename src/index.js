5dcaca73c8f4d3b94699cadf0ba338df19c88873
Olmosbek Rustamov <o.rustamov@newuu.uz>
2026-03-10 14:32:06 +0500

    add express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import { fileURLToPath } from "node:url";
-import * as path from "node:path";
+// import { fileURLToPath } from "node:url";
+// import * as path from "node:path";
+import express from "express";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(PORT);
+
 const PORT = parseInt(process.env.PORT || 3_000);
 
-console.log(__filename);
-console.log(__dirname);
-console.log(PORT)
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
+
